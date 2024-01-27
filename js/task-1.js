03e4aa88a689a9962922cc0dbaacbb6149cfe4e6
Wolf <32619379+AWoLf98@users.noreply.github.com>
2024-01-28 00:57:28 +0200

    task-6

--- js/task-1.js
+++ js/task-1.js
@@ -4,12 +4,12 @@
 // Порахує й виведе в консоль кількість категорій в ul#categories, тобто елементів li.item.
 // Для кожного елемента li.item у списку ul#categories знайде й виведе в консоль текст заголовка елемента (тегу <h2>) і кількість елементів у категорії (усіх <li>, вкладених у нього).
 
-const countItems = document.body.querySelectorAll('#categories > li.item');
+const items = document.body.querySelectorAll('#categories > li.item');
 
-console.log('Number of categories: ' + countItems.length);
+console.log('Number of categories: ' + items.length);
 
-countItems.forEach(el =>
+items.forEach(item =>
   console.log(
-    `Category: ${el.querySelector('h2').textContent}\nElements: ${el.querySelectorAll('li').length}`
+    `Category: ${item.querySelector('h2').textContent}\nElements: ${item.querySelectorAll('ul>li').length}`
   )
 );
--- js/task-6.js
+++ js/task-6.js
@@ -1,23 +1,58 @@
 'use strict';
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
 
+const buttonCreate = document.body.querySelector('#controls > button[data-create]');
+const buttonDestroy = document.body.querySelector('#controls > button[data-destroy]');
 
-// Напиши скрипт створення й очищення колекції елементів з наступним функціоналом.
+// events
+buttonCreate.addEventListener('click', function () {
+  const inputNumber = document.body.querySelector('#controls > input[type="number"]');
+  //
+  if (inputNumber.value >= 1 && inputNumber.value <= 100) {
+    createBoxes(inputNumber.value);
+  } else {
+    inputNumber.value = '';
+    alert('Bad number! min="1" max="100" step="1"');
+  }
+});
+
+buttonDestroy.addEventListener('click', destroyBoxes);
+
+//handlers
+function createBoxes(amount) {
+  const boxes = document.body.querySelector('div#boxes');
+  let boxElements = [];
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement('div');
+    let last = 30;
+
+    // в px бо браузер вимагає одиниць вимірювання для інтерпретації цього значення
+    // 1) реалізував для кожно елементу чрз значення ширини попереднього елементу
+    // 2) також можна визначати ширину дочірнього ел перед циклом, а нові обраховувати математично, але так як lastElementChild це властивість не бачу принципової різниці в цих підходах
+    if (boxes.lastElementChild !== null) {
+      const tempWidth = boxes.lastElementChild.style.width;
+      last = 10 + parseInt(tempWidth.slice(0,tempWidth.length-2));
+    }
+
+    box.style.width = box.style.height = `${10*i + last}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    boxElements.push(box);
+  }
+  boxes.append(...boxElements);
+}
+
+function destroyBoxes() {
+  document.querySelectorAll("div#boxes > div").forEach(el => el.remove());
+}
+
+// Напиши скрипт створення й очищення колекції елементів з наступним функціоналм.
 
 // Є input, у який користувач вводить бажану кількість елементів. Після натискання на кнопку Create має рендеритися (додаватися в DOM) колекція з відповідною кількістю елементів і очищатися значення в інпуті. При повторному натисканні на кнопку Create поверх старої колекції має рендеритись нова. Після натискання на кнопку Destroy колекція елементів має очищатися.
-
-// <div id="controls">
-//   <input type="number" min="1" max="100" step="1" />
-//   <button type="button" data-create>Create</button>
-//   <button type="button" data-destroy>Destroy</button>
-// </div>
-
-// <div id="boxes"></div>
-
 // Після натискання користувачем на кнопку Create треба провалідувати значення в input, воно має бути в межах від 1 до 100 включно. Тільки якщо воно задоволяє умову, мають додаватися нові <div> елементи в DOM.
 
 // Для рендеру елементів на сторінці створи функцію createBoxes(amount), яка приймає один параметр — число, що зберігає кількість елементів для рендеру. Функція має створювати стільки <div> елементів, скільки вказано в параметрі amount і додавати їх у DOM дочірніми елементами для div#boxes.
